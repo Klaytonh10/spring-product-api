8c8d96fd8031e9c9ccbb7c1bdde5fa62b4880e42
Klaytoh10 <klaytonh10@gmail.com>
2025-12-10 13:27:41 -0600

    daos and controllers for products and categories implemented

--- src/main/java/com/pluralsight/demo/controllers/CategoryController.java
+++ src/main/java/com/pluralsight/demo/controllers/CategoryController.java
@@ -1,12 +1,13 @@
 package com.pluralsight.demo.controllers;
 
-
 import com.pluralsight.demo.daos.CategoryDao;
+import com.pluralsight.demo.daos.ProductDao;
 import com.pluralsight.demo.models.Category;
+import com.pluralsight.demo.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -16,15 +17,19 @@ public class CategoryController {
 
     CategoryDao dao;
 
-    // http://localhost:8080/
-    @RequestMapping(path = "/categories", method = RequestMethod.GET)
-    public ArrayList<Category> getAllCategories() {
-        return this.dao.getCategories();
+    @Autowired
+    public CategoryController(CategoryDao dao) {
+        this.dao = dao;
     }
 
-    @RequestMapping(path = "/hello", method = RequestMethod.GET)
-    public String hello(@RequestParam(name = "greetName") String name) {
-        return "Hello " + name + "!";
+    @RequestMapping(value="/categories", method= RequestMethod.GET)
+    public ArrayList<Category> getAllCategories() {
+        return dao.getCategories();
+    }
+
+    @RequestMapping(value="/categories/{id}", method=RequestMethod.GET)
+    public Category getCategoryId(@PathVariable int id) {
+        return dao.getCategoryById(id);
     }
 
 }
